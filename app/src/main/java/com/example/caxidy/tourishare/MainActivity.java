7d1a6f35f72d931ccef1ee9628ad531fed98a962
ElenaCuellar <caxidy.x3@gmail.com>
2017-04-04 20:53:27 +0200

    UI configuration. First day.

--- app/src/main/java/com/example/caxidy/tourishare/MainActivity.java
+++ app/src/main/java/com/example/caxidy/tourishare/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.caxidy.tourishare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //!!probar, no se si funciona
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE); //para ocultar el header, que no queremos en esta actividad
         setContentView(R.layout.activity_main);
     }
 }
